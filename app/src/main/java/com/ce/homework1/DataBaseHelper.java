f0f1f4004d40066af3bce8bd453fa001de96cfa8
bardia-mhd <Bbardia2001>
2021-03-09 01:26:08 +0330

    Add insert and upgrade method to DataBaseHelper

--- app/src/main/java/com/ce/homework1/DataBaseHelper.java
+++ app/src/main/java/com/ce/homework1/DataBaseHelper.java
@@ -34,7 +34,14 @@ public class DataBaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         String createTableStatement = "CREATE TABLE " + COIN_TABLE + " (" +
-                COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COLUMN_PRICE + " DOUBLE, " + COLUMN_PERCENT_CHANGE_HOUR + " DOBLE, " + COLUMN_PERCENT_CHANGE_DAY + " DOUBLE, " + COLUMN_PERCENT_CHANGE_WEEK + " DOUBLE, " + COLUMN_NAME + " TEXT, " + COLUMN_SYMBOL + " TEXT, " + COLUMN_IMAGE_URL + " TEXT )";
+                COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_PRICE + " DOUBLE, " +
+                COLUMN_PERCENT_CHANGE_HOUR + " DOBLE, " +
+                COLUMN_PERCENT_CHANGE_DAY + " DOUBLE, " +
+                COLUMN_PERCENT_CHANGE_WEEK + " DOUBLE, " +
+                COLUMN_NAME + " TEXT, " +
+                COLUMN_SYMBOL + " TEXT, "
+                + COLUMN_IMAGE_URL + " TEXT )";
 
 
         db.execSQL(createTableStatement);
@@ -43,15 +50,11 @@ public class DataBaseHelper extends SQLiteOpenHelper {
     //version number changed
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("DROP TABLE IF EXISTS coin");
+        onCreate(db);
     }
 
-    public boolean deleteOne(){
-        //mikhad ?
-        return true;
-    }
-
-    public boolean addOne(Coin coin) {
+    public boolean insertCoin(Coin coin) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues cv = new ContentValues();
         cv.put(COLUMN_PRICE, coin.getPrice());
@@ -62,26 +65,46 @@ public class DataBaseHelper extends SQLiteOpenHelper {
         cv.put(COLUMN_SYMBOL, coin.getSymbol());
         cv.put(COLUMN_IMAGE_URL, coin.getImageUrl());
 
-        long insert = db.insert(COIN_TABLE, null, cv);
-        return insert != 1;
+        db.insert(COIN_TABLE, null, cv);
+        return true;
+
+//        long insert = db.insert(COIN_TABLE, null, cv);
+//        return insert != 1;
     }
 
-    public List<Coin> getAll(){
-        List<Coin> returnList = new ArrayList<>();
-        String queryString = "SELECT * FROM " + COIN_TABLE;
+    public Cursor getData(int id) {
         SQLiteDatabase db = this.getReadableDatabase();
-        Cursor cursor = db.rawQuery(queryString, null);
-        if (cursor.moveToFirst()) {
+        Cursor res = db.rawQuery("SELECT * FROM COIN_TABLE WHERE ID=" + COLUMN_ID + "", null);
+        return res;
+    }
 
-            do{
-                int coinID = cursor.getInt(0);
-                String coinName = cursor.getString(5);
-            }while (cursor.moveToNext());
+    public boolean updateCoin(Coin coin) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(COLUMN_PRICE, coin.getPrice());
+        cv.put(COLUMN_PERCENT_CHANGE_HOUR, coin.getPercentChangeHour());
+        cv.put(COLUMN_PERCENT_CHANGE_DAY, coin.getPercentChangeDay());
+        cv.put(COLUMN_PERCENT_CHANGE_WEEK, coin.getPercentChangeWeek());
+        cv.put(COLUMN_NAME, coin.getName());
+        cv.put(COLUMN_SYMBOL, coin.getSymbol());
+        cv.put(COLUMN_IMAGE_URL, coin.getImageUrl());
 
-        }else {
-            return null;
+        db.update(COIN_TABLE, cv, "id = ? ", new String[]{Integer.toString(coin.getId())});
+        return true;
+    }
+
+    public ArrayList<String> getAllCoins() {
+        ArrayList<String> array_list = new ArrayList<>();
+
+        //hp = new HashMap();
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res =  db.rawQuery( "select * from COIN_TABLE", null );
+        res.moveToFirst();
+
+        while(!res.isAfterLast()){
+            array_list.add(res.getString(res.getColumnIndex(COLUMN_NAME)));
+            res.moveToNext();
         }
-        cursor.close();
-        return returnList;
+        return array_list;
     }
 }
